2e2d3c1072fd34eba2f2e839d6aaa0f8f9139390
talissonaf <talisson_af@outlook.com>
2024-09-20 11:51:19 -0400

    Gerência de Configuração - atividade89863 - Ajustes no código

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-$('#inputNumero').focus().keyup(function (e) {
+$("#inputNumero").focus().keyup(function (e) {
   if (e.keyCode == 13) {
-    $('#calcular').trigger('click');
+    $("#calcular").trigger("click");
   }
 });
 
 // Adiciona um evento de clique ao botão "Calcular Fatorial"
-$('#calcular').click(function(e) {
-  const number = parseInt($('#inputNumero').val());
-  const resultElement = document.getElementById('resultado');
+$("#calcular").click(function (e) {
+  const number = parseInt($("#inputNumero").val());
+  const resultElement = document.getElementById("resultado");
 
   // Verifica se o valor inserido é um número válido e não negativo
   if (isNaN(number) || number < 0) {
-      // Exibe mensagem de erro se a entrada for inválida
-      resultElement.textContent = 'Por favor, insira um número inteiro e que não seja negativo!';
-      return;
+    // Exibe mensagem de erro se a entrada for inválida
+    resultElement.textContent = "Por favor, insira um número inteiro e que não seja negativo!";
+    return;
   }
 
   const fatorial = calculaFatorial(number);
-  
+
   // Exibe o resultado na página
   resultElement.textContent = `O fatorial de ${number} é ${fatorial}.`;
 });
@@ -25,13 +25,13 @@ $('#calcular').click(function(e) {
 // Função que calcula o fatorial de um número
 function calculaFatorial(n) {
   if (n === 0 || n === 1) {
-      return 1;
+    return 1;
   }
 
   let result = 1;
 
   for (let i = 2; i <= n; i++) {
-      result *= i;
+    result *= i;
   }
 
   return result; // Retorna o resultado final do fatorial
